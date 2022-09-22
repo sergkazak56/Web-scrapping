8a7ab07a974fb56fb86675c3c64296089f2aed4a
SergKazak <kazak5670@gmail.com>
2022-09-22 10:14:33 +0500

    Last commit

--- functions.py
+++ functions.py
@@ -3,6 +3,21 @@ import bs4
 import fake_useragent
 import re
 
+# Функция считывания разметки страницы
+def read_page(base_url='', second_url=''):
+    """
+    Функция считывания разметки страницы
+    :param base_url: базовое url страницы
+    :param second_url: вторичное url страницы
+    :return: разметку страницы
+    """
+    link = base_url + second_url
+    if link:
+        ua = fake_useragent.UserAgent()
+        response = requests.get(link, headers={'User-Agent': ua.chrome})
+        return response.text
+    return ''
+
 # Функция поиска слов в тексте
 def find_in_text(text, words, find_method):
     """
@@ -13,7 +28,7 @@ def find_in_text(text, words, find_method):
     :return: boolean
     """
     if find_method:
-        text_list = re.sub("[^\w]", " ", text).split()
+        text_list = re.sub("[^\w]", " ", text).lower().split()
         for word in words:
             if word in text_list:
                 return True
@@ -26,55 +41,52 @@ def find_in_text(text, words, find_method):
         else:
             return False
 
-# Функция парсинга по полному тексту статей на переданной странице
-def scrape_article(text, words, base_url, find_method=0):
+# Функция парсинга по тексту превью или полному тексту статей на переданной странице
+def scrape_articles(words, base_url, second_url, parsing_method=1, find_method=0):
     """
-     Функция парсинга по полному тексту статей на переданной странице
-    :param text: текст - разметка страницы
+     Функция парсинга по тексту превью или полному тексту статей на переданной странице
     :param words: список слов для поиска или регулярное выражение
     :param base_url: базовое url страницы
+    :param second_url: вторичное url страницы
+    :param parsing_method: метод парсинга 1 - по превью статей, 0 - по полному тексту статей
     :param find_method: метод поиска 1 - словами, 0 - регулярным выражением
-    :return: вывод даты публикации, названия статьи и ссылки на статью в консоль
+    :return: вывод в консоль даты публикации, названия статьи и ссылки на статью в консоль
     """
+    text = read_page(base_url, second_url)
+    if not text:
+        print('Неверно заданы url страницы')
+        return
     soup = bs4.BeautifulSoup(text, features='html.parser')
     articles = soup.find_all('article')
     print(f'Номер:  Дата, время:{" " * 9}Заголовок ---> cсылка:')
     ind = 1
-    for article in articles:
-        h2 = article.find("h2")
-        link = base_url + h2.find("a").attrs["href"]
-        ua = fake_useragent.UserAgent()
-        response = requests.get(link, headers={'User-Agent': ua.chrome})
-        art_text = response.text
-        art_soup = bs4.BeautifulSoup(art_text, features='html.parser')
-        art_body = art_soup.find('article')
-        if find_in_text(art_body.text, words, find_method):
-            art_date = article.find("time").attrs["title"]
-            h2 =  article.find("h2")
-            title = h2.find("span").text
-            href = h2.find("a").attrs["href"]
-            print(f'{ind}.{" " * (7 - len(str(ind)))}{art_date}    {title} ---> {base_url + href}')
-            ind += 1
-
-# Функция ввода, считывания страницы и вызова скрапинга
-def main():
-    """
-    Функция ввода метода поиска, считывания страницы и вызова скрапинга
-    """
-    parsing_method = int(input("Введите способ парсинга (1 - по превью статей, 0 - по полному тексту статей): "))
-    find_method = int(input("Введите способ поиска (1 - по словам, 0 - с помощью регулярного выражения): "))
-    words_list = ['программа', 'цифровая', 'Microsoft', 'Apple', 'приложение', 'IT']
-    regexp = '\s?[Пп]рограмм[а-яё]+\s?|\s?[Цц]ифр[а-яё]+\s?|\s?[Mm]icrosoft\s?|\s?[Aa]pple\s?|\s?[Пп]риложен[а-яё]\s?|\s?IT\s?'
-    base_url = 'https://habr.com'
-    page_url = base_url + '/ru/all/'
-    ua = fake_useragent.UserAgent()
-    response = requests.get(page_url, headers={'User-Agent': ua.chrome})
-    text = response.text
-    if find_method:
-        scrape_article(text, words_list, base_url, find_method)
+    if parsing_method:
+        for article in articles:
+            preview1 = article.find(class_='article-formatted-body article-formatted-body article-formatted-body_version-1')
+            preview2 = article.find(class_='article-formatted-body article-formatted-body article-formatted-body_version-2')
+            if preview1:
+                preview_txt = preview1.text
+            elif preview2:
+                preview_txt = preview2.text
+            else:
+                preview_txt = ''
+            if find_in_text(preview_txt, words, find_method):
+                art_date = article.find("time").attrs["title"]
+                h2 = article.find("h2")
+                title = h2.find("span").text
+                href = h2.find("a").attrs["href"]
+                print(f'{ind}.{" " * (7 - len(str(ind)))}{art_date}    {title} ---> {base_url + href}')
+                ind += 1
     else:
-        scrape_article(text, regexp, base_url, find_method)
-
-
-if __name__ == '__main__':
-    main()
+        for article in articles:
+            h2 = article.find("h2")
+            art_text = read_page(base_url, h2.find("a").attrs["href"])
+            art_soup = bs4.BeautifulSoup(art_text, features='html.parser')
+            art_body = art_soup.find('article')
+            if find_in_text(art_body.text, words, find_method):
+                art_date = article.find("time").attrs["title"]
+                h2 =  article.find("h2")
+                title = h2.find("span").text
+                href = h2.find("a").attrs["href"]
+                print(f'{ind}.{" " * (7 - len(str(ind)))}{art_date}    {title} ---> {base_url + href}')
+                ind += 1
